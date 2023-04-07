5cd2423287b68b086894a87fd3c26452290bd1e7
bog <bog@gozmail.net>
2023-04-07 10:28:11 +0200

    RM: sentry-debug view removed as sentry works.

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -3,15 +3,11 @@ from django.urls import path
 
 from . import views
 
-def trigger_error(request):
-    div_by_zero = 1/0
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', views.lettings_index, name='lettings_index'),
     path('lettings/<int:letting_id>/', views.letting, name='letting'),
     path('profiles/', views.profiles_index, name='profiles_index'),
     path('profiles/<str:username>/', views.profile, name='profile'),
-    path('admin/', admin.site.urls),
-    path('sentry-debug/', trigger_error)
+    path('admin/', admin.site.urls)
 ]
